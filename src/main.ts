e057d82b08d101b48cff3132721d98dc23618c2a
akuma19 <kellyhandy31@gmail.com>
2026-03-02 21:05:34 +0100

    feat:connected to database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
+const logger = new Logger('Bootstrap');
 async function bootstrap() {
+  const PORT = process.env.PORT || 8000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+  logger.log(`Application is running on: http://localhost:${PORT}`);
 }
 bootstrap();
