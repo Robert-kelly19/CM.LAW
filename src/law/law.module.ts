10f2c9e8210b7e16f9cb87038185c1198539c4c3
akuma19 <kellyhandy31@gmail.com>
2026-03-06 15:50:14 +0100

    app started

--- src/law/law.module.ts
+++ src/law/law.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LawService } from './law.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [LawService],
   exports: [LawService],
 })
